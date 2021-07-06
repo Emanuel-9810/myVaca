a59460f5e6152dae42a6e8afa88615d80cd6cc9c
Emanuel-9810 <60586959+Emanuel-9810@users.noreply.github.com>
2021-07-06 09:38:49 -0700

    Vacation app update to destinationlist

--- app/src/main/java/com/myandroidhello/myvaca/User.java
+++ app/src/main/java/com/myandroidhello/myvaca/User.java
@@ -2,11 +2,19 @@ package com.myandroidhello.myvaca;
 
 public class User {
     public String email;
+    public String locations;
 
     public User(){}
 
+    //Login cnstrctr
     public User(String email){
         this.email = email;
+    }
+
+    //Cnstrctr for loc
+    public User(String email, String locations){
+        this.email = email;
+        this.locations = locations;
     }
 }
 
